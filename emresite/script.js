a0ed085e38fcc62523a7ce47db6d444f4d5e8a99
easthvacr <154758744+easthvacr@users.noreply.github.com>
2023-12-25 01:19:59 +0300

    Add files via upload

---
+++ emresite/script.js
@@ -0,0 +1,11 @@
+
+        function scrollToElement() {
+            // Get the reference to the target element
+            var targetElement = document.getElementById("details-1");
+
+            // Scroll to the target element
+            
+               targetElement.scrollIntoView();
+             
+        }
+  
